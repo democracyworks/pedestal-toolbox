b778a17d9d548929de228b1575ae44cac59dd90b
Chris Shea <cmshea@gmail.com>
2014-02-03 15:33:16 -0500

    let-or-reply
    Usage: (let-or-reply ctx [bindings*] exprs*)
    
    binding => binding-form test response
    
    Like if-let, but permits multiple bindings and associates the
    response onto the pedestal context if a test fails

---
+++ test/turbovote/pedestal_toolbox/macros_test.clj
@@ -0,0 +1,46 @@
+(ns turbovote.pedestal-toolbox.macros-test
+  (:require [clojure.test :refer :all]
+            [turbovote.pedestal-toolbox.macros :refer :all]))
+
+(deftest let-or-reply-test
+  (let [context {:request {:foo 1 :bar "hi"}}]
+    (testing "with empty bindings, just does exprs"
+      (let [side-effects (atom 0)
+            new-context (let-or-reply context []
+                                      (swap! side-effects inc)
+                                      (assoc-in context [:request :baz] :good))]
+        (is (= (assoc-in context [:request :baz] :good) new-context))
+        (is (= 1 @side-effects))))
+    (testing "with multiple sets of bindings that all succeed"
+      (let [good-side-effects (atom 0)
+            fail-side-effects (atom 0)
+            new-context (let-or-reply context
+                                      [foo (get-in context [:request :foo])
+                                           (do (swap! fail-side-effects inc)
+                                               :failed-foo)
+                                       bar (get-in context [:request :bar])
+                                           (do (swap! fail-side-effects inc)
+                                               :failed-bar)]
+                                      (swap! good-side-effects inc)
+                                      (assoc-in context [:request :baz] :great))]
+        (is (= (assoc-in context [:request :baz] :great) new-context))
+        (is (zero? @fail-side-effects))
+        (is (= 1 @good-side-effects))))
+    (testing "with multiple sets of bindings with one that fails"
+      (let [good-side-effects (atom 0)
+            fail-side-effects (atom 0)
+            new-context (let-or-reply context
+                                      [foo (get-in context [:request :foo])
+                                           (do (swap! fail-side-effects inc)
+                                               :failed-foo)
+                                       nope (:nope context)
+                                            (do (swap! fail-side-effects inc)
+                                                :failed-nope)
+                                       bar (get-in context [:request :bar])
+                                           (do (swap! fail-side-effects inc)
+                                               :failed-bar)]
+                                      (swap! good-side-effects inc)
+                                      (assoc-in context [:request :baz] :wont-see))]
+        (is (= (assoc context :response :failed-nope) new-context))
+        (is (zero? @good-side-effects))
+        (is (= 1 @fail-side-effects))))))
