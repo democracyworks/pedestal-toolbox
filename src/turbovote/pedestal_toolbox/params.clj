1a66f40c68d19745d752bbcba7f7608b22e0f3e4
Chris Shea <cmshea@gmail.com>
2014-02-12 16:58:04 -0500

    [CS NH] Coerce and validate schemas together
    prismatic/schema can do it! Let's us remove a bunch of code.

--- src/turbovote/pedestal_toolbox/params.clj
+++ src/turbovote/pedestal_toolbox/params.clj
@@ -2,7 +2,21 @@
   (:require [io.pedestal.service.http.body-params :as body-params]
             [io.pedestal.service.interceptor :refer [defbefore interceptor]]
             [turbovote.pedestal-toolbox.response :as response]
-            [schema.core :as s]))
+            [schema.core :as s]
+            [schema.coerce :as coerce]
+            [clj-time.coerce :as t]))
+
+(defn coerce-or-identity [f]
+  (fn [x]
+    (try
+      (or (f x) x)
+      (catch Exception e x))))
+
+(let [coercions {java.util.UUID (coerce-or-identity #(java.util.UUID/fromString %))
+                 java.util.Date (coerce-or-identity t/to-date)}]
+  (defn coercion-matcher [schema]
+    (or (coercions schema)
+        (coerce/json-coercion-matcher schema))))
 
 (defbefore body-params
   [ctx]
@@ -11,25 +25,6 @@
     (catch Exception e
       (assoc ctx :response (response/bad-request (.getMessage e))))))
 
-(defn coerce-body-params
-  "Takes a map of coercion fns that potentially raise clojure.lang.ExceptionInfo
-   if the coercion is not possible with the provided input."
-  ([] (coerce-body-params {}))
-  ([coercions]
-     (interceptor
-      :enter
-      (fn [ctx]
-        (loop [param-keys [:edn-params :json-params]]
-          (if (empty? param-keys)
-            ctx
-            (let [param-key (first param-keys)]
-              (if-let [params (get-in ctx [:request param-key])]
-                (try (assoc-in ctx [:request :body-params]
-                               ((get coercions param-key identity) params))
-                     (catch clojure.lang.ExceptionInfo e
-                       (assoc ctx :response (response/bad-request (.getMessage e)))))
-                (recur (rest param-keys))))))))))
-
 (defn validate-params
   [param-key schema]
   (with-meta
@@ -37,8 +32,13 @@
      :enter
      (fn [ctx]
        (try
-         (s/validate schema (get-in ctx [:request param-key]))
-         ctx
+         (let [validator (coerce/coercer schema coercion-matcher)
+               params (validator (get-in ctx [:request param-key]))]
+           (if-let [error (schema.utils/error-val params)]
+             (assoc ctx :response (response/bad-request
+                                   (str "Value does not match schema: "
+                                        error)))
+             (assoc-in ctx [:request param-key] params)))
          (catch clojure.lang.ExceptionInfo e
            (assoc ctx :response (response/bad-request (.getMessage e)))))))
     {:schema schema}))
