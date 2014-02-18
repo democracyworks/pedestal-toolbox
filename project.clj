97933639c64dc3c6fda9c0211605398eb7911fbc
Chris Shea <cmshea@gmail.com>
2014-02-18 14:42:58 -0500

    [CS NH] 0.3.1

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject turbovote.pedestal-toolbox "0.3.1-SNAPSHOT"
+(defproject turbovote.pedestal-toolbox "0.3.1"
   :description "Pedestal service helpers"
   :url "http://www.github.com/turbovote/pedestal-toolbox"
   :license {:name "Eclipse Public License"
