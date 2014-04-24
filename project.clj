918a755c9eb88fd78394e6be8c308b991fef820f
Wes Morgan <cap10morgan@gmail.com>
2014-04-24 11:10:43 -0600

    [WM] update to Clojure 1.6.0

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
   :url "http://www.github.com/turbovote/pedestal-toolbox"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.5.1"]
+  :dependencies [[org.clojure/clojure "1.6.0"]
                  [io.pedestal/pedestal.service "0.2.2"]
                  [liberator "0.10.0"]
                  [ring/ring-core "1.2.1"]
