bbfba422881c38eea866e31c0b1af6b124bac581
Nathan Herzing <nherzing@gmail.com>
2014-06-25 15:54:54 -0400

    [NH] Bump to 0.3.4

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject turbovote.pedestal-toolbox "0.3.4-SNAPSHOT"
+(defproject turbovote.pedestal-toolbox "0.3.4"
   :description "Pedestal service helpers"
   :url "http://www.github.com/turbovote/pedestal-toolbox"
   :license {:name "Eclipse Public License"
