f8e923978b41ca081fcab2ce3da632fa1e37b8af
Nathan Herzing <nherzing@gmail.com>
2014-06-25 15:54:15 -0400

    [NH] Upgrade to pedestal 0.3.0

--- project.clj
+++ project.clj
@@ -4,9 +4,9 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]
-                 [io.pedestal/pedestal.service "0.2.2"]
+                 [io.pedestal/pedestal.service "0.3.0"]
                  [liberator "0.11.0"]
-                 [ring/ring-core "1.2.2"]
+                 [ring/ring-core "1.3.0"]
                  [cheshire "5.3.1"]
                  [prismatic/schema "0.2.1"]
                  [clj-time "0.7.0"]])
