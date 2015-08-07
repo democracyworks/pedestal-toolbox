7e9ccb9fb79a8bd865a72a5b57da0cdc7493c89d
Wes Morgan <cap10morgan@gmail.com>
2015-08-07 14:49:49 -0600

    [WM] set clojars to default release repo

--- project.clj
+++ project.clj
@@ -10,4 +10,5 @@
                  [cheshire "5.5.0"]
                  [prismatic/schema "0.4.3"]
                  [clj-time "0.10.0"]
-                 [com.cognitect/transit-clj "0.8.275"]])
+                 [com.cognitect/transit-clj "0.8.275"]]
+  :deploy-repositories [["releases" :clojars]])
