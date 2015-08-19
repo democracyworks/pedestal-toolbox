7f372ef4cbf8d02babaebd36ad0f6d4eba1ec692
Chris Shea <cmshea@gmail.com>
2015-08-19 10:12:11 -0400

    Version 0.6.2

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject democracyworks/pedestal-toolbox "0.6.2-SNAPSHOT"
+(defproject democracyworks/pedestal-toolbox "0.6.2"
   :description "Pedestal service helpers"
   :url "http://www.github.com/democracyworks/pedestal-toolbox"
   :license {:name "Eclipse Public License"
