087bef07e92add002d90094538223c6cc8f45721
Eric Normand <ericwnormand@gmail.com>
2016-02-05 15:56:01 -0600

    [EN SD] Convert domain-matcher-fn to take regexes

--- src/pedestal_toolbox/cors.clj
+++ src/pedestal_toolbox/cors.clj
@@ -1,7 +1,6 @@
 (ns pedestal-toolbox.cors)
 
-(defn domain-matcher-fn [list-of-strings]
-  (let [patterns (map re-pattern list-of-strings)]
-    (fn [origin]
-      (boolean
-       (some #(re-matches % origin) patterns)))))
+(defn domain-matcher-fn [patterns]
+  (fn [origin]
+    (boolean
+     (some #(re-matches % origin) patterns))))
--- test/pedestal_toolbox/cors_test.clj
+++ test/pedestal_toolbox/cors_test.clj
@@ -4,25 +4,25 @@
 
 (deftest cors-domain-matcher-fn-test
   (testing "make sure beta.turbovote.org is allowed"
-    (let [f (domain-matcher-fn ["https://beta[.]turbovote[.]org"])]
+    (let [f (domain-matcher-fn [#"https://beta[.]turbovote[.]org"])]
       (is (true? (f "https://beta.turbovote.org")))
       (is (false? (f "https://ourtime-beta.turbovote.org")))))
   (testing "make sure turbovote.org domains are allowed"
-    (let [f (domain-matcher-fn ["https://(.+[.])?turbovote[.]org"])]
+    (let [f (domain-matcher-fn [#"https://(.+[.])?turbovote[.]org"])]
       (is (true? (f "https://beta.turbovote.org")))
       (is (true? (f "https://ourtime-beta.turbovote.org")))
       (is (false? (f "http://google.com")))
       (is (true? (f "https://turbovote.org")))))
   (testing "make sure wildcard domain works"
-    (let [f (domain-matcher-fn [".+"])]
+    (let [f (domain-matcher-fn [#".+"])]
       (is (true? (f "https://beta.turbovote.org")))
       (is (true? (f "https://ourtime-beta.turbovote.org")))
       (is (true? (f "http://localhost:8080")))
       (is (true? (f "http://localdocker:8080")))
       (is (false? (f "")))))
   (testing "make sure turbovote.org domains are allowed"
-    (let [f (domain-matcher-fn ["https://(.+[.])?turbovote[.]org"
-                                "http://vote[.]donaldtrump[.]com"])]
+    (let [f (domain-matcher-fn [#"https://(.+[.])?turbovote[.]org"
+                                #"http://vote[.]donaldtrump[.]com"])]
       (is (true? (f "https://beta.turbovote.org")))
       (is (true? (f "https://ourtime-beta.turbovote.org")))
       (is (false? (f "http://google.com")))
