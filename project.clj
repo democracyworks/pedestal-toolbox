c375a35c9c45465edef5a452295c33b2037f3ec1
Wes Morgan <cap10morgan@gmail.com>
2016-02-05 15:52:15 -0700

    [WM] Update deps

--- project.clj
+++ project.clj
@@ -1,14 +1,14 @@
-(defproject democracyworks/pedestal-toolbox "0.6.3-SNAPSHOT"
+(defproject democracyworks/pedestal-toolbox "0.7.0-SNAPSHOT"
   :description "Pedestal service helpers"
   :url "http://www.github.com/democracyworks/pedestal-toolbox"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.7.0"]
-                 [io.pedestal/pedestal.service "0.4.0"]
-                 [liberator "0.13"]
+                 [io.pedestal/pedestal.service "0.4.1"]
+                 [liberator "0.14.0"]
                  [ring/ring-core "1.4.0"]
                  [cheshire "5.5.0"]
-                 [prismatic/schema "0.4.3"]
-                 [clj-time "0.10.0"]
-                 [com.cognitect/transit-clj "0.8.275"]]
+                 [prismatic/schema "1.0.4"]
+                 [clj-time "0.11.0"]
+                 [com.cognitect/transit-clj "0.8.285"]]
   :deploy-repositories [["releases" :clojars]])
