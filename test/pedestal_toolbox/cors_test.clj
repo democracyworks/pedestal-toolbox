aad8ff60fa551b1015b4ba79a24fb872c3d9b1db
Eric Normand <ericwnormand@gmail.com>
2016-02-05 15:33:36 -0600

    [EN SD] Update regexes for CORS Origin format
    According to the Chrome Developer Tools, these headers are sent in with
    CORS requests.
    
    ```
    Origin: http://localhost:8080 (when testing locally)
    ```
    ```
    Origin: https://beta.turbovote.org (on the beta site)
    ```
    
    We've corrected the regexes to account for these changes.
    
    Pedestal uses the entire value of the Origin header to make the
    comparison.

--- test/pedestal_toolbox/cors_test.clj
+++ test/pedestal_toolbox/cors_test.clj
@@ -4,25 +4,29 @@
 
 (deftest cors-domain-matcher-fn-test
   (testing "make sure beta.turbovote.org is allowed"
-    (let [f (domain-matcher-fn ["beta[.]turbovote[.]org"])]
-      (is (true? (f "beta.turbovote.org")))
-      (is (false? (f "ourtime-beta.turbovote.org")))))
+    (let [f (domain-matcher-fn ["https://beta[.]turbovote[.]org"])]
+      (is (true? (f "https://beta.turbovote.org")))
+      (is (false? (f "https://ourtime-beta.turbovote.org")))))
   (testing "make sure turbovote.org domains are allowed"
-    (let [f (domain-matcher-fn ["(.*[.])?turbovote[.]org"])]
-      (is (true? (f "beta.turbovote.org")))
-      (is (true? (f "ourtime-beta.turbovote.org")))
-      (is (false? (f "google.com")))
-      (is (true? (f "turbovote.org")))))
+    (let [f (domain-matcher-fn ["https://(.+[.])?turbovote[.]org"])]
+      (is (true? (f "https://beta.turbovote.org")))
+      (is (true? (f "https://ourtime-beta.turbovote.org")))
+      (is (false? (f "http://google.com")))
+      (is (true? (f "https://turbovote.org")))))
   (testing "make sure wildcard domain works"
     (let [f (domain-matcher-fn [".+"])]
-      (is (true? (f "beta.turbovote.org")))
-      (is (true? (f "ourtime-beta.turbovote.org")))
+      (is (true? (f "https://beta.turbovote.org")))
+      (is (true? (f "https://ourtime-beta.turbovote.org")))
+      (is (true? (f "http://localhost:8080")))
+      (is (true? (f "http://localdocker:8080")))
       (is (false? (f "")))))
   (testing "make sure turbovote.org domains are allowed"
-    (let [f (domain-matcher-fn ["(.+[.])?turbovote[.]org"
-                                "vote[.]donaldtrump[.]com"])]
-      (is (true? (f "beta.turbovote.org")))
-      (is (true? (f "ourtime-beta.turbovote.org")))
-      (is (false? (f "google.com")))
-      (is (true? (f "turbovote.org")))
-      (is (true? (f "vote.donaldtrump.com"))))))
+    (let [f (domain-matcher-fn ["https://(.+[.])?turbovote[.]org"
+                                "http://vote[.]donaldtrump[.]com"])]
+      (is (true? (f "https://beta.turbovote.org")))
+      (is (true? (f "https://ourtime-beta.turbovote.org")))
+      (is (false? (f "http://google.com")))
+      (is (false? (f "https://google.com")))
+      (is (true? (f "https://turbovote.org")))
+      (is (false? (f "http://turbovote.org")))
+      (is (true? (f "http://vote.donaldtrump.com"))))))
