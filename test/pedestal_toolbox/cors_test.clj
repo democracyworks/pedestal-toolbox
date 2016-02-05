45adff0a00442dc1e3336e6f4251c6cb0bda9153
Eric Normand <ericwnormand@gmail.com>
2016-02-05 14:50:06 -0600

    [EN SD] Add more tests.

--- test/pedestal_toolbox/cors_test.clj
+++ test/pedestal_toolbox/cors_test.clj
@@ -5,12 +5,24 @@
 (deftest cors-domain-matcher-fn-test
   (testing "make sure beta.turbovote.org is allowed"
     (let [f (domain-matcher-fn ["beta[.]turbovote[.]org"])]
-      (is (true? (f "beta.turbovote.org")))))
-  (testing "make sure ourtime-beta.turbovote.org is allowed"
-    (let [f (domain-matcher-fn [".*[.]turbovote[.]org"])]
       (is (true? (f "beta.turbovote.org")))
-      (is (true? (f "ourtime-beta.turbovote.org")))))
-  (testing "make sure ourtime-beta.turbovote.org is allowed"
-    (let [f (domain-matcher-fn [".*"])]
+      (is (false? (f "ourtime-beta.turbovote.org")))))
+  (testing "make sure turbovote.org domains are allowed"
+    (let [f (domain-matcher-fn ["(.*[.])?turbovote[.]org"])]
       (is (true? (f "beta.turbovote.org")))
-      (is (true? (f "ourtime-beta.turbovote.org"))))))
+      (is (true? (f "ourtime-beta.turbovote.org")))
+      (is (false? (f "google.com")))
+      (is (true? (f "turbovote.org")))))
+  (testing "make sure wildcard domain works"
+    (let [f (domain-matcher-fn [".+"])]
+      (is (true? (f "beta.turbovote.org")))
+      (is (true? (f "ourtime-beta.turbovote.org")))
+      (is (false? (f "")))))
+  (testing "make sure turbovote.org domains are allowed"
+    (let [f (domain-matcher-fn ["(.+[.])?turbovote[.]org"
+                                "vote[.]donaldtrump[.]com"])]
+      (is (true? (f "beta.turbovote.org")))
+      (is (true? (f "ourtime-beta.turbovote.org")))
+      (is (false? (f "google.com")))
+      (is (true? (f "turbovote.org")))
+      (is (true? (f "vote.donaldtrump.com"))))))
