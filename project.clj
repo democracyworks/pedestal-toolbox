882c1f2cb070c909e0fafbdb846eeb01067a872d
Sean Duckett <sduckett@gmail.com>
2017-11-06 16:39:36 -0700

    clojure 1.7 -> clojure 1.8
    and update other ancient dependencies

--- project.clj
+++ project.clj
@@ -3,12 +3,12 @@
   :url "http://www.github.com/democracyworks/pedestal-toolbox"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.7.0"]
-                 [io.pedestal/pedestal.service "0.4.1"]
-                 [liberator "0.14.0"]
-                 [ring/ring-core "1.4.0"]
-                 [cheshire "5.5.0"]
-                 [prismatic/schema "1.0.4"]
-                 [clj-time "0.11.0"]
-                 [com.cognitect/transit-clj "0.8.285"]]
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [io.pedestal/pedestal.service "0.5.3"]
+                 [liberator "0.15.1"]
+                 [ring/ring-core "1.6.3"]
+                 [cheshire "5.8.0"]
+                 [prismatic/schema "1.1.7"]
+                 [clj-time "0.14.0"]
+                 [com.cognitect/transit-clj "0.8.300"]]
   :deploy-repositories [["releases" :clojars]])
