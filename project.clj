304e7f7f4975104fe6f7fba05a59f75863c79019
Egg Syntax <egg@novonon.com>
2019-03-14 14:21:09 -0400

    Update rest of deps with no apparent issues

--- project.clj
+++ project.clj
@@ -1,14 +1,14 @@
-(defproject democracyworks/pedestal-toolbox "0.7.1-SNAPSHOT"
+(defproject democracyworks/pedestal-toolbox "0.7.2-SNAPSHOT"
   :description "Pedestal service helpers"
   :url "http://www.github.com/democracyworks/pedestal-toolbox"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.7.0"]
-                 [io.pedestal/pedestal.service "0.4.1"]
+  :dependencies [[org.clojure/clojure "1.9.0"]
+                 [io.pedestal/pedestal.service "0.5.5"]
                  [liberator "0.15.2"]
-                 [cheshire "5.5.0"]
-                 [prismatic/schema "1.0.4"]
-                 [clj-time "0.11.0"]
-                 [com.cognitect/transit-clj "0.8.285"]]
                  [ring/ring-core "1.7.1"]
+                 [cheshire "5.8.1"]
+                 [prismatic/schema "1.1.10"]
+                 [clj-time "0.15.1"]
+                 [com.cognitect/transit-clj "0.8.313"]]
   :deploy-repositories [["releases" :clojars]])
